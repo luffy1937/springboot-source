0f67daf6d86b05b2a0da319701fb30e4cc7ed0e2
liuyuefeng <liuyf935@chinaunicom.cn>
2020-09-07 14:30:54 +0800

    @PropertySource属性设置

--- src/main/java/com/example/springbootsource/SpringbootSourceApplication.java
+++ src/main/java/com/example/springbootsource/SpringbootSourceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import java.util.Properties;
 @SpringBootApplication
 @RestController
 @Import(MyBeanImport.class)
+@PropertySource({"demo.properties"})
 public class SpringbootSourceApplication {
 	@Autowired
 	TestService testService;
